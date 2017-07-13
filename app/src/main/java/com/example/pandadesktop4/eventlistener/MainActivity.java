83f2c01d3606530ce7f063124773284cd819579b
PandaDesktop4 <komalabak@gmail.com>
2017-07-13 14:01:49 +0530

    android2

--- app/src/main/java/com/example/pandadesktop4/eventlistener/MainActivity.java
+++ app/src/main/java/com/example/pandadesktop4/eventlistener/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.pandadesktop4.eventlistener;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button b1,b2;
+    private ProgressDialog progress;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        b1 = (Button) findViewById(R.id.button);
+        b2 = (Button) findViewById(R.id.button2);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView txt = (TextView) findViewById(R.id.textView);
+                txt.setTextSize(25);
+            }
+        });
+
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView txt = (TextView) findViewById(R.id.textView);
+                txt.setTextSize(55);
+            }
+        });
+
     }
 }
