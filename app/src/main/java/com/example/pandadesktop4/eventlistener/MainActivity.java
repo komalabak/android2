48438b1779f330dd870b54eff68167239d359133
PandaDesktop4 <komalabak@gmail.com>
2017-07-13 17:13:34 +0530

    android2

--- app/src/main/java/com/example/pandadesktop4/eventlistener/MainActivity.java
+++ app/src/main/java/com/example/pandadesktop4/eventlistener/MainActivity.java
@@ -1,16 +1,37 @@
 package com.example.pandadesktop4.eventlistener;
 
+import android.app.Activity;
+import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
+//import android.view.View;
+//import android.view.View;
+//import android.view.View;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends Activity {
     Button b1;
     private ProgressDialog progress;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,22 +39,39 @@ public class MainActivity extends AppCompatActivity {
 
 
         b1 = (Button) findViewById(R.id.button);
-       // b2 = (Button) findViewById(R.id.button2);
+        // b2 = (Button) findViewById(R.id.button2);
 
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-           //     TextView txt = (TextView) findViewById(R.id.textView);
-         //       txt.setTextSize(25);
-                Toast.makeText(MainActivity.this, "Good Afternoon", Toast.LENGTH_SHORT).show();
+                //     TextView txt = (TextView) findViewById(R.id.textView);
+                //       txt.setTextSize(25);
+                //Toast.makeText(MainActivity.this, "Good Afternoon", Toast.LENGTH_SHORT).show();
+                addnotification();
             }
+
+
         });
-
-
-       // b2.setOnClickListener(new View.OnClickListener() {
-         //   @Override
-           // public void onClick(View view) {
-             //   TextView txt = (TextView) findViewById(R.id.textView);
-          //
     }
+
+    private void addnotification() {
+        android.support.v4.app.NotificationCompat.Builder builder = new NotificationCompat.Builder(this).setSmallIcon(R.drawable.n).setContentTitle("Notification Example").setContentText("This is a test notification");
+
+        Intent i = new Intent(this, MainActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(this, 0, i, PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(pi);
+
+
+        NotificationManager m = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        m.notify(0, builder.build());
+
+
+    }
+
+    // b2.setOnClickListener(new View.OnClickListener() {
+    //   @Override
+    // public void onClick(View view) {
+    //   TextView txt = (TextView) findViewById(R.id.textView);
+    //
 }
+
---
+++ app/src/main/java/com/example/pandadesktop4/eventlistener/Notification.java
@@ -0,0 +1,17 @@
+package com.example.pandadesktop4.eventlistener;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by pandadesktop4 on 13/7/17.
+ */
+
+public class Notification extends Activity{
+    public void onCreate(Bundle saveInstanceState)
+
+    {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.notification);
+    }
+}
