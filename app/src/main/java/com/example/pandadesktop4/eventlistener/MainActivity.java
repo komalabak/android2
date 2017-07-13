fb0c3a12753d502509f3f2f00adbca03fc58c780
PandaDesktop4 <komalabak@gmail.com>
2017-07-13 11:35:58 +0530

    Android2

---
+++ app/src/main/java/com/example/pandadesktop4/eventlistener/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.pandadesktop4.eventlistener;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button b1,b2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
